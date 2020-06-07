01a56b10de6948ae7f6ad05dfe5241729f466656
admin <846884240@qq.com>
2020-06-07 14:57:52 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -15,9 +15,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:cn-sy-yl/cn-sy-yl.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:cn-sy-yl/vblog.git master:gh-pages
+#git push -f git@github.com:cn-sy-yl/vblog.git master:gh-pages
 
 cd -
